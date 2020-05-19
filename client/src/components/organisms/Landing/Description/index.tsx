b0feceb4317975ef1eec3beedb2f4f35bef0a259
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-19 12:59:36 +0200

    Add welcome and description

--- client/src/components/organisms/Landing/Description/index.tsx
+++ client/src/components/organisms/Landing/Description/index.tsx
@@ -1,17 +1,71 @@
-import React from 'react';
-import { withTranslation } from 'react-i18next';
+import React from "react";
+import { withTranslation, Trans } from "react-i18next";
 
-import Button from 'components/atoms/Button/Button';
+import Button from "components/atoms/Button/Button";
+
+import "./style.scss";
 
 const Description = (props) => {
   const { t } = props;
 
   return (
-    <div className="Landing__container__body">
-      Explaination
-    <Button text={t('landing.buttonText')}  className="Button_white" onClickAction={props.onClickNext}/>
-  </div>
-  )
-}
+    <div className="Description">
+      <div className="Description__logo">
+        <img src="/images/logo/logo_gauche_blanc.png" alt="logo" />
+      </div>
+      <div className="Description__body">
+        <div className="Description__body__elt">
+          <div className="Description__body__elt__img">
+            <img
+              src="images/rewards/greenlantern-planet-10x.png"
+              alt="greenlantern-planet-10x"
+            />
+          </div>
+          <div className="Description__body__elt__text">
+            <Trans i18nKey="landing.description.medals">
+              25 <b>médailles</b> à collectionner pour exhiber votre talent
+              d'explorateur de la collaboration !
+            </Trans>
+          </div>
+        </div>
+        <div className="Description__body__elt">
+          <div className="Description__body__elt__img">
+            <img src="images/rewards/cup-10x.png" alt="cup-10x" />
+          </div>
+          <div className="Description__body__elt__text">
+            <Trans i18nKey="landing.description.questions">
+              200 <b>quesstions</b> pour vous propulser sur Microsoft Teams
+            </Trans>
+          </div>
+        </div>
+
+        <div className="Description__body__elt">
+          <div className="Description__body__elt__img">
+            <img src="images/rewards/gummystars-10x.png" alt="gummystars-10x" />
+          </div>
+          <div className="Description__body__elt__text">
+            <Trans i18nKey="landing.description.reward">
+              Une <b> récompense d'honneur </b> pour le meilleur explorateur"
+            </Trans>
+          </div>
+        </div>
+
+        <div className="Description__body__elt">
+          <div className="Description__body__elt__img">
+            <img src="images/rewards/chest-10x.png" alt="chest-10x" />
+          </div>
+          <div className="Description__body__elt__text">
+            {t("landing.description.gifts")}
+          </div>
+        </div>
+      </div>
+      <Button
+        text={t("landing.description.buttonText")}
+        className="Button__white col-3"
+        onClickAction={props.onClickNext}
+      />
+    </div>
+  );
+};
 
 export default withTranslation()(Description);
