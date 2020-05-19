23c1c0422c1ece5488d168d0fc8f286420208e09
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-19 12:59:36 +0200

    Add welcome and description

--- client/src/components/organisms/Landing/Welcome/index.tsx
+++ client/src/components/organisms/Landing/Welcome/index.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { withTranslation } from 'react-i18next';
+import React from "react";
+import { withTranslation } from "react-i18next";
 
-import Button from 'components/atoms/Button/Button';
+import Button from "components/atoms/Button/Button";
 
-import './style.scss';
+import "./style.scss";
 
 const Welcome = (props) => {
   const { t } = props;
 
   return (
     <div className="Welcome col-12">
-      <span className="Welcome__heading">{t('landing.welcome_text')}</span>
+      <span className="Welcome__heading">{t("landing.welcome.text")}</span>
       <div className="Welcome__logo">
         <img src="/images/logo/logo_gauche_blanc.png" alt="logo" />
       </div>
-      <span className="Welcome__body">{t('landing.body')}</span>
-      <Button text={t('landing.buttonText')}  className="Welcome__button Button__white col-3" onClickAction={props.onClickNext}/>
-  </div>
-  )
-}
+      <span className="Welcome__body">{t("landing.welcome.body")}</span>
+      <Button
+        text={t("landing.welcome.buttonText")}
+        className="Welcome__button Button__white col-3"
+        onClickAction={props.onClickNext}
+      />
+    </div>
+  );
+};
 
 export default withTranslation()(Welcome);
