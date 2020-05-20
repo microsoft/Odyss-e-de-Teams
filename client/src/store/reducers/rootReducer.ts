f20a119eafa70dfad9e19eac654b090d724ff8b6
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-20 10:23:46 +0200

    remove redux for avatars (not necessary)

--- client/src/store/reducers/rootReducer.ts
+++ client/src/store/reducers/rootReducer.ts
@@ -2,14 +2,12 @@ import { combineReducers } from "redux";
 
 import { questionReducer } from "./questionReducer";
 import { userReducer } from "./userReducer";
-import { avatarReducer } from "./avatarReducer";
 
 import IStore from "../IStore";
 
 const rootReducer = combineReducers<IStore>({
   question: questionReducer,
   user: userReducer,
-  avatars: avatarReducer,
 });
 
 export default rootReducer;
