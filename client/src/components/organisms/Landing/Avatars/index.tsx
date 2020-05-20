5be1bc4d77a725fa5171d74f1b630a1fed399107
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-20 10:23:21 +0200

    add avatar logic

--- client/src/components/organisms/Landing/Avatars/index.tsx
+++ client/src/components/organisms/Landing/Avatars/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React from "react";
 
-const Avatars = (props) => (
-  <h1>Avatars</h1>
-)
+import { withTranslation } from "react-i18next";
 
-export default Avatars;
+import Button from "components/atoms/Button/Button";
+import AvatarCard from "components/molecules/AvatarCard";
+
+import "./style.scss";
+
+const Avatars = (props) => {
+  console.log("props", props);
+  const { t, onSelectAvatar, avatars, onClickNext } = props;
+
+  const onSelect = (id, event) => {
+    event.preventDefault();
+    onSelectAvatar(id);
+  };
+  return (
+    <div className="Avatars">
+      <div className="Avatars__title">{t("landing.Avatars.title")}</div>
+      <div className="Avatars__subtitle">{t("landing.Avatars.subtitle")}</div>
+      <div className="Avatars__body">
+        <div className="Avatars__body__title">
+          {t("landing.Avatars.body.title")}
+        </div>
+
+        <div className="Avatars__body__subtitle">
+          {t("landing.Avatars.body.description")}
+        </div>
+
+        <div className="Avatars__body__list">
+          {avatars.map((avatar, index) => (
+            <AvatarCard
+              className="Avatars__body__list"
+              key={index}
+              id={avatar.id_avatar}
+              title={avatar.nom}
+              description={avatar.description}
+              image={`/images/avatar/${avatar.image}`}
+              onClickActionText={t("landing.Avatars.body.selectAvatar")}
+              selected={avatar.selected}
+              onClickAvatarAction={onSelect}
+            />
+          ))}
+        </div>
+
+        <div className="Avatars__actions">
+          <Button
+            text={t("landing.Avatars.buttonText")}
+            className="Button__white col-3"
+            onClickAction={onClickNext}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withTranslation()(Avatars);
