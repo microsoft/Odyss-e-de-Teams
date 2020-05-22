5396cabc6465eec2e695dcf4c40ff6069eaf96b9
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-22 11:42:45 +0200

    add mobile version for welcome component

--- client/src/components/organisms/Landing/Welcome/index.tsx
+++ client/src/components/organisms/Landing/Welcome/index.tsx
@@ -6,7 +6,7 @@ import Button from "components/atoms/Button/Button";
 import "./style.scss";
 
 const Welcome = (props) => {
-  const { t } = props;
+  const { t, isMobile } = props;
 
   return (
     <div className="Welcome col-12">
@@ -14,10 +14,17 @@ const Welcome = (props) => {
       <div className="Welcome__logo">
         <img src="/images/logo/logo_gauche_blanc.png" alt="logo" />
       </div>
-      <span className="Welcome__body">{t("landing.welcome.body")}</span>
+
+      <span className="Welcome__body">
+        {isMobile
+          ? t("landing.welcome.body-mobile")
+          : t("landing.welcome.body")}
+      </span>
       <Button
         text={t("landing.welcome.buttonText")}
-        className="Welcome__button Button__white col-3"
+        className={`Welcome__button ${
+          isMobile ? "Button__orange-gradiant" : "Button__white"
+        } col-sm-3 col-10`}
         onClickAction={props.onClickNext}
       />
     </div>
