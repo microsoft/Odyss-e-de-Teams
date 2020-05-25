0a709c57be0656e344869dcfe4a2d5c296c9e282
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-25 17:33:07 +0200

    redo all apis

---
+++ client/src/api/Classement.ts
@@ -0,0 +1,23 @@
+import API from "./ApiMain";
+
+class Classement extends API {
+  protected resource = "/classement";
+
+  getClassement(
+    lang: string,
+    mode: string,
+    monde: number = 0,
+    user: number = 0
+  ): Promise<any> {
+    console.log(process.env);
+
+    return super.fetchGET(this.resource, {
+      language: lang,
+      mode: mode,
+      monde: monde,
+      user: user,
+    });
+  }
+}
+
+export default new Classement();
