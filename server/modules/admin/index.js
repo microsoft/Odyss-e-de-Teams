c1c537b81def3b104a2523f4098c50b1c9e693dd
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-26 18:00:26 +0200

    add current mission api

--- server/modules/admin/index.js
+++ server/modules/admin/index.js
@@ -8,11 +8,35 @@ const register = async (server, options) => {
     method: "GET",
     handler: function (request, h) {
       const db = request.getDb("odyssee_teams");
-      let replacements = { id_organisation: 1 };
+      let replacements = { id_organisation: 1 }; // TODO: change id with session user orga
 
       return db.sequelize
         .query(
           "SELECT COUNT(id_user) as cnt_user FROM t_user WHERE id_organisation =:id_organisation",
+          {
+            replacements: replacements,
+            type: QueryTypes.SELECT,
+          }
+        )
+        .then((result) => {
+          return {
+            results: result[0],
+          };
+        });
+    },
+  });
+
+  //missions en cours
+  server.route({
+    path: "/admin/current-mission",
+    method: "GET",
+    handler: function (request, h) {
+      const db = request.getDb("odyssee_teams");
+      let replacements = { id_organisation: 1 }; // TODO: change id with session user orga
+
+      return db.sequelize
+        .query(
+          "select ts.nom as mission_name, ts.horodatage + INTERVAL '14 day' as mission_end from t_organisation org inner join t_semaine ts  on ts.id_semaine = org.id_semaine where id_organisation =:id_organisation",
           {
             replacements: replacements,
             type: QueryTypes.SELECT,
