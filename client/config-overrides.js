6fe00bd9a14ab3562b60f637d750f10232baa7ca
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-26 14:41:56 +0200

    add more path aliases

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -6,6 +6,10 @@ module.exports = function override(config) {
     alias: {
       components: path.resolve(__dirname, "src", "components"),
       api: path.resolve(__dirname, "src", "api"),
+      layouts: path.resolve(__dirname, "src", "layouts"),
+      containers: path.resolve(__dirname, "src", "containers"),
+      models: path.resolve(__dirname, "src", "models"),
+      store: path.resolve(__dirname, "src", "store"),
     },
   };
   return config;
