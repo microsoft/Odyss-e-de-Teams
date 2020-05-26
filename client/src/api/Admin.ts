89acf313d8fecf2c8a48d8dce1a6e832a9f2dde3
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-26 14:43:27 +0200

    add apis for explorers

---
+++ client/src/api/Admin.ts
@@ -0,0 +1,11 @@
+import API from "./ApiMain";
+
+class Admin extends API {
+  protected resource = "/admin";
+
+  getExplorersCount(): Promise<any> {
+    return super.fetchGET(this.resource + "/explorers-count");
+  }
+}
+
+export default new Admin();
