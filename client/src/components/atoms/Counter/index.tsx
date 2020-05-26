7a31d943cc5fe5b7577669be1ce95e2e72b8fb46
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-26 17:59:25 +0200

    add counter

---
+++ client/src/components/atoms/Counter/index.tsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import moment from "moment";
+
+interface ICountdownProps {
+  timeTillDate: string;
+  timeFormat: string;
+  className?: string;
+}
+
+interface ICountdownState {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
+class Countdown extends Component<ICountdownProps, ICountdownState> {
+  state = {
+    days: undefined,
+    hours: undefined,
+    minutes: undefined,
+    seconds: undefined,
+  };
+
+  interval = null;
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { timeTillDate, timeFormat } = this.props;
+      const then = moment(timeTillDate, timeFormat);
+      const now = moment();
+      const countdown = moment(Number(then) - Number(now));
+      const days = countdown.format("D");
+      const hours = countdown.format("HH");
+      const minutes = countdown.format("mm");
+      const seconds = countdown.format("ss");
+
+      this.setState({ days, hours, minutes, seconds });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+    const { className } = this.props;
+
+    return (
+      <div className={className}>
+        {days}j {hours}:{minutes}:{seconds}
+      </div>
+    );
+  }
+}
+
+export default Countdown;
