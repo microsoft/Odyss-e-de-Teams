7547d6de470d1dc2154decb32c20993ad2e06e2e
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-27 15:45:33 +0200

    refacto app container

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -5,21 +5,29 @@ import { Container } from "react-bootstrap";
 
 import "./App.scss";
 
-import Menu from "../Menu/Menu";
-import Cockpit from "../Cockpit/Cockpit";
-import Header from "../../components/Header/Header";
-import Landing from "../Landing/index";
-import Profil from "../Profil/Profil";
-import Classement from "../Classement/Classement";
+import Header from "components/Header/Header";
+
+// containers
+import Menu from "containers/Menu/Menu";
+import Cockpit from "containers/Cockpit/Cockpit";
+import Landing from "containers/Landing/index";
+import Profil from "containers/Profil/Profil";
+import Classement from "containers/Classement/Classement";
+
+//layouts
+import MainLayout from "layouts/Main";
+
+import Admin from "containers/Admin";
 
 import User from "api/User";
 
-import IStore from "../../store/IStore";
-import { IAppProps, IAppState } from "../../models/App";
+import IStore from "store/IStore";
+import { IAppProps, IAppState } from "models/App";
 
 class App extends React.Component<IAppProps, IAppState> {
   state = {
     logged: true,
+    is_admin: true,
   };
 
   componentDidMount() {
@@ -47,54 +55,71 @@ class App extends React.Component<IAppProps, IAppState> {
 
     const isMobile = window.innerWidth <= 768;
 
-    const { logged } = this.state;
+    const { logged, is_admin } = this.state;
 
-    const toRender = !logged ? (
-      <Landing onCompleteLanding={this.onCompleteLanding} isMobile={isMobile} />
-    ) : (
-      <Container
-        fluid
-        className={`${
-          hasGradient ? "gradient" : ""
-        } main-container h-100 d-flex p-0`}
-      >
-        <Menu currentRouterLink={this.props.location.pathname} />
-        <div className={"main-content w-100 py-4 px-5"}>
-          <div className={"mb-3"}>
-            <Header hasGradient={hasGradient} />
+    let toRender = null;
+
+    if (!logged)
+      toRender = (
+        <Landing
+          onCompleteLanding={this.onCompleteLanding}
+          isMobile={isMobile}
+        />
+      );
+
+    // admin
+    if (logged && is_admin) {
+      toRender = <Admin />;
+    }
+
+    // user
+    if (logged && !is_admin) {
+      toRender = (
+        <Container
+          fluid
+          className={`${
+            hasGradient ? "gradient" : ""
+          } main-container h-100 d-flex p-0`}
+        >
+          <Menu currentRouterLink={this.props.location.pathname} />
+          <div className={"main-content w-100 py-4 px-5"}>
+            <div className={"mb-3"}>
+              <Header hasGradient={hasGradient} />
+            </div>
+            <Switch>
+              <Route exact path="/Cockpit">
+                <Cockpit />
+              </Route>
+              <Route exact path="/Jouer">
+                <p>Jouer container</p>
+              </Route>
+              <Route exact path="/Classement">
+                <Classement />
+              </Route>
+              <Route exact path="/Profil">
+                <Profil />
+              </Route>
+              <Route exact path="/Regles">
+                <p>Regles container</p>
+              </Route>
+              <Route exact path="/Planning">
+                <p>Planning container</p>
+              </Route>
+              <Route exact path="/Outillage">
+                <p>Outillage container</p>
+              </Route>
+              <Route exact path="/Landing">
+                <Landing
+                  onCompleteLanding={this.onCompleteLanding}
+                  isMobile={isMobile}
+                />
+              </Route>
+            </Switch>
           </div>
-          <Switch>
-            <Route exact path="/Cockpit">
-              <Cockpit />
-            </Route>
-            <Route exact path="/Jouer">
-              <p>Jouer container</p>
-            </Route>
-            <Route exact path="/Classement">
-              <Classement />
-            </Route>
-            <Route exact path="/Profil">
-              <Profil />
-            </Route>
-            <Route exact path="/Regles">
-              <p>Regles container</p>
-            </Route>
-            <Route exact path="/Planning">
-              <p>Planning container</p>
-            </Route>
-            <Route exact path="/Outillage">
-              <p>Outillage container</p>
-            </Route>
-            <Route exact path="/Landing">
-              <Landing
-                onCompleteLanding={this.onCompleteLanding}
-                isMobile={isMobile}
-              />
-            </Route>
-          </Switch>
-        </div>
-      </Container>
-    );
+        </Container>
+      );
+    }
+
     return toRender;
   }
 }
