b517e1e0c7239c27f1fa88ed8c900250c8f584c0
thibault.leroy <thibault.le-roy@saegus.com>
2020-05-28 11:25:22 +0200

    change main to stateless component

--- client/src/layouts/Main/index.tsx
+++ client/src/layouts/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 
 interface IMainProps {
@@ -6,24 +6,17 @@ interface IMainProps {
   children: any;
 }
 
-class Main extends Component<IMainProps, {}> {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { hasGradient, children } = this.props;
-    return (
-      <Container
-        fluid
-        className={`${
-          hasGradient ? "gradient" : ""
-        } main-container h-100 d-flex p-0`}
-      >
-        {children}
-      </Container>
-    );
-  }
-}
-
+const Main = (props: IMainProps) => {
+  const { hasGradient, children } = props;
+  return (
+    <Container
+      fluid
+      className={`${
+        hasGradient ? "gradient" : ""
+      } main-container h-100 d-flex p-0`}
+    >
+      {children}
+    </Container>
+  );
+};
 export default Main;
