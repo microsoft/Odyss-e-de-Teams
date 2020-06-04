23c7ffdb8c96b61bbce071511b5e09e32d4120b7
thibault.leroy <thibault.le-roy@saegus.com>
2020-06-04 15:26:23 +0200

    add semaine model

---
+++ server/models/semaine/index.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+  const SemaineModel = sequelize.define(
+    "Semaine",
+    {
+      id_semaine: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nom: DataTypes.STRING,
+      description: DataTypes.STRING,
+      ordre: DataTypes.NUMBER,
+      actif: DataTypes.BOOLEAN,
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "horodatage",
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "horodatage_creation",
+      },
+    },
+    {
+      tableName: "t_semaine",
+    }
+  );
+
+  return SemaineModel;
+};
