9d14160764a61981e3e8971add51c80cf655cc74
thibault.leroy <thibault.le-roy@saegus.com>
2020-06-09 16:12:30 +0200

    finish agenda

--- client/src/components/molecules/Admin/AgendaWeekSwitcher/index.tsx
+++ client/src/components/molecules/Admin/AgendaWeekSwitcher/index.tsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "react-bootstrap";
+
+import { withTranslation, WithTranslation } from "react-i18next";
+
+import "./style.scss";
 
 interface IAgendaWeekSwitcher {
   onClickPreviousWeek: any;
   onClickNextWeek: any;
+  previousAvailable: boolean;
+  nextAvailable: boolean;
+  semaines: any;
+  currentWeek: any;
 }
 
-const AgendaWeekSwitcher = (props) => {
+const AgendaWeekSwitcher = (props: IAgendaWeekSwitcher & WithTranslation) => {
+  const {
+    tReady,
+    t,
+    currentWeek,
+    semaines,
+    previousAvailable,
+    nextAvailable,
+  } = props;
+
   return (
     <div className="AgendaWeekSwitcher">
-      <div className="AgendaWeekSwitcher__previous">
-        <Button onClick={props.onClickPreviousWeek}> &lt; </Button>
+      <div className={`AgendaWeekSwitcher__previous`}>
+        <Button
+          onClick={props.onClickPreviousWeek}
+          disabled={!previousAvailable}
+        >
+          {" "}
+          &lt;{" "}
+        </Button>
       </div>
 
-      <div className="AgendaWeekSwitcher__current">Semaine 1: Lancement</div>
+      <div className="AgendaWeekSwitcher__current">
+        {tReady && t("admin.agenda.week")} {currentWeek + 1}{" "}
+        {semaines[currentWeek].name}
+      </div>
 
       <div className="AgendaWeekSwitcher__next">
-        <Button onClick={props.onClickNextWeek}> &gt; </Button>
+        <Button onClick={props.onClickNextWeek} disabled={!nextAvailable}>
+          {" "}
+          &gt;{" "}
+        </Button>
       </div>
     </div>
   );
 };
 
-export default AgendaWeekSwitcher;
+export default withTranslation()(AgendaWeekSwitcher);
