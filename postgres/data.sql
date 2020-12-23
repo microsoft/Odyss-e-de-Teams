aeb1741eba3b4e20001350413aa62ca30f321cdb
eBauman-factory <67316441+eBauman-factory@users.noreply.github.com>
2020-12-23 11:29:51 +0100

    ajout des reponse en dur

--- postgres/data.sql
+++ postgres/data.sql
@@ -366,6 +366,9 @@ INSERT INTO public.t_agenda (nom, description, id_semaine, num_jour, heure, acti
     FROM public.i_question a
         INNER JOIN public.t_question b ON TRIM(a.code_module)=TRIM(b.cle_fichier)
 	UNION ALL
+	/* TODO: inserer les vraies réponse en anglais quans on les aura */
+	SELECT DISTINCT 'REPONSE', id_reponse, 'en', nom, NULL::text FROM public.t_reponse
+	UNION ALL
 	SELECT DISTINCT 'QUESTION', id_question, 'fr', nom, commentaire FROM public.t_question
 	UNION ALL
 	SELECT DISTINCT 'REPONSE', id_reponse, 'fr', nom, NULL::text FROM public.t_reponse
