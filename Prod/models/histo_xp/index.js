d1d46b92aa9977d0644d332aad145d086d948ac8
Eric BAUMAN <eric.bauman@saegus.com>
2022-01-07 17:11:32 +0100

    ajout dossier prod

---
+++ Prod/models/histo_xp/index.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "HGainXP",
+    {
+      id_gain_xp: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      id_user: DataTypes.INTEGER,
+      nb_point: DataTypes.INTEGER,
+
+      horodatage: DataTypes.DATE,
+    },
+    {
+      tableName: "h_gain_xp",
+      timestamps: false,
+    }
+  );
---
+++ Prod/models/organisation_agenda/index.js
@@ -0,0 +1,43 @@
+module.exports = function (sequelize, DataTypes) {
+  const OrganisationAgenda = sequelize.define(
+    "OrganisationAgenda",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      id_organisation: DataTypes.INTEGER,
+      id_agenda: DataTypes.INTEGER,
+      id_semaine: DataTypes.INTEGER,
+      date_event: DataTypes.DATE,
+      done: DataTypes.BOOLEAN,
+    },
+    {
+      tableName: "j_organisation_agenda",
+      timestamps: false,
+    }
+  );
+
+  OrganisationAgenda.associate = (models) => {
+    OrganisationAgenda.hasOne(models.Agenda, {
+      foreignKey: "id_agenda",
+      sourceKey: "id_agenda",
+    });
+    OrganisationAgenda.hasOne(models.Organisation, {
+      foreignKey: "id_organisation",
+      sourceKey: "id_organisation",
+    });
+    OrganisationAgenda.hasOne(models.Semaine, {
+      foreignKey: "id_semaine",
+      sourceKey: "id_semaine",
+    });
+
+    OrganisationAgenda.hasOne(models.OrganisationSemaine, {
+      foreignKey: "id_semaine",
+      sourceKey: "id_semaine",
+    });
+  };
+
+  return OrganisationAgenda;
+};
